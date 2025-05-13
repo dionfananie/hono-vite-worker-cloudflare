0ec490021aa3014a45076648e83ec1c53f723271
Dion Fananie <dionfananie@gmail.com>
2025-05-13 12:42:29 +0700

    env

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 export default defineConfig({
+  define: {
+    "process.env.VITE_URL_WS": JSON.stringify(process.env.VITE_URL_WS),
+  },
   server: {
     allowedHosts: ["twiki-invention-you-boost.trycloudflare.com"],
   },
